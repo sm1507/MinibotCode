65683c6be7577bcb7b91b6218b711c610bfe462f
Gabriel McMillan <fearxzombie@icloud.com>
2020-01-03 14:07:37 -0800

    Removed SpeedControllerGroup and TalonSRX imports as they are not nessasary for this purpose

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -11,11 +11,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DriveConstants;
 
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
 public class driveSubsystem extends SubsystemBase {
   /**
