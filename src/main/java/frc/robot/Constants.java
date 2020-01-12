12850337f177643a0528705076c2e87e8a7f2a87
Bryn Dole <bryn.dole@gmail.com>
2020-01-12 15:41:29 -0800

    auton constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -58,4 +58,13 @@ public final class Constants {
     public static final class OIConstants {
         public static final int kDriverController = 0;
     }
+
+    public static final class AutoConstants {
+        public static final double kMaxSpeedMetersPerSecond = 3;
+        public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+    
+        // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
+        public static final double kRamseteB = 2;
+        public static final double kRamseteZeta = 0.7;
+      }
 }
