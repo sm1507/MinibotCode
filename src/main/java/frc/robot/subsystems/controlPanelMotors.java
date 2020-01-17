05a24e8ae81124db5a7a764b859a5eed2413862a
jestsh <jestsh@users.noreply.github.com>
2020-01-16 19:12:22 -0800

    Added some things from example code

--- src/main/java/frc/robot/subsystems/controlPanelMotors.java
+++ src/main/java/frc/robot/subsystems/controlPanelMotors.java
@@ -9,11 +9,30 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
 public class controlPanelMotors extends SubsystemBase {
   /**
    * Creates a new controlPanelMotors.
    */
+  private static final int kMotorPort = 0;
+  private static final int kJoystickPort = 0;
+  private static final int kEncoderPortA = 0;
+  private static final int kEncoderPortB = 1;
+
+  private SpeedController m_motor;
+  private Joystick m_joystick;
+  private Encoder m_encoder;
+
+
   public controlPanelMotors() {
+
 
   }
 
