a3752b5526c15bfe210d0da0336145c414d30ba3
Gabriel McMillan <fearxzombie@icloud.com>
2020-01-17 18:19:21 -0800

    Made Constants.Gains visible to driveCommand

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -26,5 +26,5 @@ public static final class DriveConstants {
 public static final int kSlotIdx = 0;
 public static final int kPIDLoopIdx = 0;
 public static final int kTimeoutMs = 30;
-static final Gains kGains = new Gains(0.2, 0.0, 0.0, 0.2, 0, 1.0);
+public static final Gains kGains = new Gains(0.2, 0.0, 0.0, 0.2, 0, 1.0);
 }
