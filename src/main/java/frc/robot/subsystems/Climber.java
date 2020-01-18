acd3582531852893c4c2d73056d238ded3e2452b
LiamPresler <57422255+LiamPresler@users.noreply.github.com>
2020-01-18 12:27:47 -0800

    more progress

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -7,32 +7,45 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
-import edu.wpi.first.wpilibj.SpeedController;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.elevatorConstants;
 
 public class Climber extends SubsystemBase {
 
-  private final WPI_TalonFX ClimbFalcon1;
-  private final WPI_TalonFX ClimbFalcon2;
-  private final SpeedController BothSides;
+  private final WPI_TalonFX elevator1 = new WPI_TalonFX(elevatorConstants.elevator1);
+  private final WPI_TalonFX elevator2 = new WPI_TalonFX(elevatorConstants.elevator2);
+  private final WPI_TalonFX elevatorWinch = new WPI_TalonFX(elevatorConstants.elevatorWinch);
 
   /**
    * Creates a new Climber.
    */
   public Climber() {
-    ClimbFalcon1 = new WPI_TalonFX(DriveConstants.ClimbFalcon1);
-    ClimbFalcon2 = new WPI_TalonFX(DriveConstants.ClimbFalcon2);
-    ClimbFalcon2.setInverted(true);
-    BothSides = new SpeedControllerGroup(ClimbFalcon1, ClimbFalcon2);
+    elevator1.configFactoryDefault();
+    elevator2.configFactoryDefault();
+    elevator2.follow(elevator1);
+    elevator2.setInverted(true);
+    elevator1.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, elevatorConstants.elevatorSlotIdx, elevatorConstants.elevatorPivotTimeout);
+    elevator1.setSensorPhase(true);
+    elevator1.configNominalOutputForward(0, elevatorConstants.elevatorPivotTimeout);
+    elevator1.configNominalOutputReverse(0, elevatorConstants.elevatorPivotTimeout);
+    elevator1.configPeakOutputForward(1, elevatorConstants.elevatorPivotTimeout);
+    elevator1.configPeakOutputReverse(-1, elevatorConstants.elevatorPivotTimeout);
+    elevator1.setNeutralMode(NeutralMode.Coast);
+    elevator2.setNeutralMode(NeutralMode.Coast);
   }
 
-  public void deployArm() {
-    // TODO: code motor to deploy arm
+  public void setelevatorRPM() {
+    //TODO: elevator1.set(ControlMode.Position,  );Arm Deployment
   }
-
-
+public void setelevatorPID(double P, double I, double D, double F){
+  elevator1.config_kP(elevatorConstants.elevatorSlotIdx, P, elevatorConstants.elevatorPivotTimeout);
+  elevator1.config_kP(elevatorConstants.elevatorSlotIdx, I, elevatorConstants.elevatorPivotTimeout);
+  elevator1.config_kP(elevatorConstants.elevatorSlotIdx, D, elevatorConstants.elevatorPivotTimeout);
+  elevator1.config_kP(elevatorConstants.elevatorSlotIdx, F, elevatorConstants.elevatorPivotTimeout);
+}
 }
